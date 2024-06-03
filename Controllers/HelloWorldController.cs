76c49551b669d5359b979f6f2a957c0c5a3c0c15
ragineeguptta <ragineegupta123@gmail.com>
2024-06-03 14:30:19 +0530

    Part 2, add a controller to an ASP.NET Core MVC app

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+        //https://localhost:{PORT}/HelloWorld/Welcome?name=Rick&numtimes=4
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+
+        //https://localhost:{PORT}/HelloWorld/Welcome/3?name=Rick
+        public string Welcome2(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
