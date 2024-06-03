01c3997087d7623e4de825dc3844b267a0572ff0
ragineeguptta <ragineegupta123@gmail.com>
2024-06-03 14:55:06 +0530

    Part 3, add a view to an ASP.NET Core MVC app

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -7,22 +7,19 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         // GET: /HelloWorld/
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
         // GET: /HelloWorld/Welcome/ 
         //https://localhost:{PORT}/HelloWorld/Welcome?name=Rick&numtimes=4
-        public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
         }
 
-        //https://localhost:{PORT}/HelloWorld/Welcome/3?name=Rick
-        public string Welcome2(string name, int ID = 1)
-        {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
-        }
     }
 }
